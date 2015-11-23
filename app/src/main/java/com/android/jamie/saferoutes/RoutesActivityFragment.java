b623df69340c3da51ae32a782965241132e77179
Jamie <jamie_mcd23@hotmail.com>
2015-11-23 00:13:38 +0000

    Http request for directions added

--- app/src/main/java/com/android/jamie/saferoutes/RoutesActivityFragment.java
+++ app/src/main/java/com/android/jamie/saferoutes/RoutesActivityFragment.java
@@ -2,8 +2,11 @@ package com.android.jamie.saferoutes;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +36,12 @@ import java.util.Set;
 public class RoutesActivityFragment extends Fragment {
 
     public RoutesActivityFragment() {
+    }
+
+    public void onStart(){
+        super.onStart();
+        FetchRoutes myFetchRoutes = new FetchRoutes();
+        myFetchRoutes.execute("");
     }
 
     @Override
@@ -40,16 +59,6 @@ public class RoutesActivityFragment extends Fragment {
                 "Dummy data - Blah - DUMMY",
                 "Dummy data - Blah - DUMMY",
                 "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY",
-                "Dummy data - Blah - DUMMY"
         };
 
         List<String> dummyList = new ArrayList<String>(Arrays.asList(data));
@@ -72,5 +81,86 @@ public class RoutesActivityFragment extends Fragment {
        String end = pref.getString(getString(R.string.end_point), null);
 
         return rootView;
+    }
+
+    public class FetchRoutes extends AsyncTask<String, Void, JSONObject>{
+
+        private final String LOG_TAG = FetchRoutes.class.getSimpleName();
+
+        /*  Example request
+            https://maps.googleapis.com/maps/api/directions/json?origin=Brooklyn&destination=Queens&departure_time=1343641500&mode=transit
+         */
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String start = pref.getString(getString(R.string.start_point), null);
+            String end = pref.getString(getString(R.string.end_point), null);
+
+            try {
+                String startLocation = start;
+                String destination = end;
+                String alternatives = "true";
+                String mode = "walking";
+
+                final String ORIGIN= "origin=";
+                final String DESTINATION = "&destination=";
+                final String ALTERNATIVES = "&alternatives=";
+                final String MODE = "&mode=";
+                //final String API_KEY = "key";
+                final String BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?";
+
+                StringBuilder directionsURL = new StringBuilder(300);
+
+                directionsURL.append(BASE_URL);
+                directionsURL.append(ORIGIN);
+                directionsURL.append(startLocation);
+                directionsURL.append(DESTINATION);
+                directionsURL.append(destination);
+                directionsURL.append(MODE);
+                directionsURL.append(mode);
+                directionsURL.append(ALTERNATIVES);
+                directionsURL.append(alternatives);
+
+                URL url = new URL(directionsURL.toString().replace(" ",""));
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+          }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // print out the completed buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+
+            }catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+
+        }
     }
 }
